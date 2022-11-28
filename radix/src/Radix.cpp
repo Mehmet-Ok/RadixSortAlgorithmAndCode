442ed757ffb9e4009b0646c9f18c4f6b8dc47923
Mehmet-Ok <104078116+Mehmet-Ok@users.noreply.github.com>
2022-11-29 01:02:09 +0300

    radix.cpp son

--- radix/src/Radix.cpp
+++ radix/src/Radix.cpp
@@ -32,3 +32,61 @@ Radix::Radix(int* numbers, int lenght){
     maxStep = maxStepNumber();
 }
 
+int* Radix::queueCurrenLenght(){
+    int* lenghts = new int[10];
+    for(int i = 0; i < 10; i++)
+        lenghts[i] = queues[i]->count();
+    return lenghts;
+}
+
+int* Radix::sort(){
+    int numIndex = stepValue = num =0;
+
+    //dizinin bir kere isleme alinmasi
+    for(; numIndex<lenght; numIndex++)
+    {
+        stepValue = numbers[numIndex] % 10;
+        num = numbers[numIndex];
+        queues[stepValue]->enqueue(num);
+    }
+
+    stepValue = num =0;
+    for(int i = 1;i<maxStep; i++)
+    {
+        int* qCurrenLenghts = queueCurrenLenght();
+        for(int index = 0; index<10; index++)
+        {
+            int len = qCurrenLenghts[index];
+            for(;len>0;len--)
+            {
+                num = queues[index]->peek();
+                queues[index]->dequeue();
+                stepValue = (num/(int)pow(10,i))%10;
+                queues[stepValue]->enqueue(num);
+            }
+        }
+        delete [] qCurrenLenghts;
+    }
+
+    int* ordered = new int[lenght];
+    numIndex = 0;
+    num = 0;
+    for(int i = 0; i<10; i++)
+    {
+        while(!queues[i]->isEmpty()){
+            num =queues[i]->peek();
+            ordered[numIndex] = num;
+            numIndex++;
+            queues[i]->dequeue();
+        }
+    }
+    return ordered;
+}
+
+Radix::~Radix(){
+    delete [] numbers;
+    for(int i = 0; i<10; i++)
+        delete queues[i];
+    delete [] queues;
+}
+
