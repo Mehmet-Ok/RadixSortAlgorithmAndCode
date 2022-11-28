57d7fbadef27a719cf86d8817ff5f6d658ae56ff
Mehmet-Ok <104078116+Mehmet-Ok@users.noreply.github.com>
2022-11-29 00:12:05 +0300

    radix kurucu

--- radix/src/Radix.cpp
+++ radix/src/Radix.cpp
@@ -1,7 +1,7 @@
 #include "Radix.hpp"
 
 //private
-int Radix::maxStepNumber(){
+int Radix::maxStepNumber(){ 
     int max = 0;
     for(int i = 0; i<lenght;i++)
         if(stepCount(numbers[i])>max) max = stepCount(numbers[i]);
@@ -21,3 +21,14 @@ int Radix::stepCount(int num){
 
 
 //public
+Radix::Radix(int* numbers, int lenght){
+    this->numbers = numbers;
+    this->lenght = lenght;
+    for(int i = 0; i<lenght; i++)
+        this->numbers[i] = numbers[i];
+    Queue **queues = new Queue*[10];
+    for(int i = 0; i < 10; i++)
+        queues[i] = new Queue();
+    maxStep = maxStepNumber();
+}
+
